3e22038169f1a548a685c994ef4151ccefce513b
md-rafi-3-4 <mdrafiislam9411@gmail.com>
2025-08-10 17:53:37 +0600

    updated

--- index.js
+++ index.js
@@ -279,6 +279,21 @@ async function run() {
     res.send(result)
   })
 
+  app.get("/all-blogs",async(req,res)=>{
+    const result=await blogsCollections.find({status:"active"}).toArray()
+    res.send(result)
+  })
+
+  app.get("/blog/:id",async(req,res)=>{
+    const {id}=req.params;
+    // console.log(id)
+    const query={_id: new ObjectId(id)}
+    const result=await blogsCollections.findOne(query)
+    res.send(result)
+  })
+
+
+
 
 
 
